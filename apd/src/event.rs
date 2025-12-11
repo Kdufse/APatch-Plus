9282b63fb0d6cf86fee065c232df1d609f3a14ff
无影. <qq840908178@163.com>
2025-12-11 22:21:21 +0800

    默认启用OverlayFS

--- apd/src/event.rs
+++ apd/src/event.rs
@@ -518,7 +518,9 @@ pub fn on_services(superkey: Option<String>) -> Result<()> {
 fn run_uid_monitor() {
     info!("Trigger run_uid_monitor!");
 
-    let mut command = &mut Command::new("/data/adb/apd");
+    let mut command = Command::new("sh");
+    command.arg("-c")
+       .arg("touch /data/adb/.overlayfs_enable && /data/adb/apd");
     {
         command = command.process_group(0);
         command = unsafe {

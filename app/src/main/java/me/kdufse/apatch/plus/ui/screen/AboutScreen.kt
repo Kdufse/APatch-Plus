d470ac15c7f0870eba9a5879523b2f38a226cf86
无影. <qq840908178@163.com>
2025-12-13 12:49:41 +0800

    修复APP_Language代码

--- app/src/main/java/me/kdufse/apatch/plus/ui/screen/AboutScreen.kt
+++ app/src/main/java/me/kdufse/apatch/plus/ui/screen/AboutScreen.kt
@@ -106,7 +106,7 @@ fun AboutScreen(navigator: DestinationsNavigator) {
                 horizontalArrangement = Arrangement.Center
             ) {
                 FilledTonalButton(
-                    onClick = { uriHandler.openUri("https://github.com/Kdufse/APatch-Plus") }
+                    onClick = { uriHandler.openUri("https://github.com/matsuzaka-yuki/FolkPatch") }
                 ) {
                     Icon(
                         painter = painterResource(id = R.drawable.github),
@@ -119,7 +119,7 @@ fun AboutScreen(navigator: DestinationsNavigator) {
                 Spacer(modifier = Modifier.width(10.dp))
 
                 FilledTonalButton(
-                    onClick = { uriHandler.openUri("https://t.me/APatchPlus") }
+                    onClick = { uriHandler.openUri("https://t.me/FolkPatch") }
                 ) {
                     Icon(
                         painter = painterResource(id = R.drawable.telegram),

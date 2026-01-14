02a7393f576febeb94e62d119ef19a9ea1084622
kse <qq840908178@163.com>
2026-01-14 20:48:24 +0800

    使用XZPatch

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     alias(libs.plugins.kotlin.compose.compiler) apply false
 }
 
-project.ext.set("kernelPatchVersion", "0.12.6")
+project.ext.set("kernelPatchVersion", "0.13.0")
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 36
@@ -32,7 +32,7 @@ fun getGitDescribe(): String {
 }
 
 fun getVersionCode(): Int {
-    return 112130
+    return 112152
 }
 
 fun getbranch(): String {
@@ -40,7 +40,7 @@ fun getbranch(): String {
 }
 
 fun getVersionName(): String {
-    return "1.8.1"
+    return "1.8.2"
 }
 
 tasks.register("printVersion") {
